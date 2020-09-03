53763e2580e299f89fe1bffd7d2871e134048b6f
moorthybasava <moorthybasava@hotmail.com>
2020-09-03 19:06:37 +0530

    Login page feature added --login feature UI is designed and developed as per the specs in the assignment --functionality to check for blank username and password added --functionality to check for incorrect credentials added --material ui components used --Login.js and Login.css modified

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './common/Header.js';
+import Login from './screens/login/Login.js'; 
+import 'typeface-roboto';
+
 
 ReactDOM.render(
     <span>
-        <Header/>
+        <Login/>
     </span>, 
     document.getElementById('root')
 );
